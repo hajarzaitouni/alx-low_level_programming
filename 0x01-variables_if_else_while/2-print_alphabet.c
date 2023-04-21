bf75a9a34211cbe31e1c75500c4ea06d0eac701f
hajar zaitouni <hajarzaitouni88@gmail.com>
2023-04-21 16:03:38 -0700

    add print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print alphabet in lowercase
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+
+	return (0);
+}
