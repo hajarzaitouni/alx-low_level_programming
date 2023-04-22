6167e4e51b084ee4a57da24c89dc68abac63c160
hajar zaitouni <hajarzaitouni88@gmail.com>
2023-04-21 17:28:45 -0700

    add print_numberz File

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints digit using putchar and int type
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int digit_n = 0;
+
+	while (digit_n <= 9)
+	{
+		/*convert into ASCII code*/
+		putchar(48 + n);
+		digit_n++;
+	}
+	putchar('\n');
+
+	return (0);
+}
