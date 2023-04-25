f6ae8c7693f14b0c8d18824ca68d368b74ea86ea
hajar zaitouni <hajarzaitouni88@gmail.com>
2023-04-25 12:57:28 -0700

    add print_alphabet_x10 File

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase 
+ * 			using _putchar function
+ */
+
+void print_alphabet_x10(void)
+{
+	int line, i;
+
+	for (line = 0; line < 10; line++)
+	{
+		for (i = 97; i < 122; i++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
+	}
+}
