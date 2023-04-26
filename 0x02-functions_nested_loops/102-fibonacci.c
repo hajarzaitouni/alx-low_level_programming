7ba906cdd44a6c04260781eec3c21ae6704fb9fa
hajar zaitouni <hajarzaitouni88@gmail.com>
2023-04-26 14:56:45 -0700

    add fibonacci File

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints the first 50 Fibonacci numbers
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n, i;
+	unsigned long fib1, fib2, fib3;
+
+	n = 50;
+	fib1 = fib2 = 1;
+
+	for (i = 0; i < n; i++)
+	{
+		fib3 = fib2 + fib1;
+		printf("%d", fib3);
+
+		fib1 = fib2;
+		fib2 = fib3;
+		if (i != n - 1)
+			printf(", ");
+		else
+			printf("\n");
+	}
+
+	return (0);
+}
