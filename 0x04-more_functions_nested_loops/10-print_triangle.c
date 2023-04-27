a0660c1207691ff328d8ee0b7ea7e58976ccd3cf
hajar zaitouni <hajarzaitouni88@gmail.com>
2023-04-27 15:00:50 -0700

    update main.h and add 10-print_triangle.c

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a triangle
+ * @size: the size of the triangle
+ */
+
+void print_triangle(int size)
+{
+	int i, j;
+
+	for (i = 1; i <= size; i++)
+	{
+		for (j = 1; j <= size - i; j++)
+		{
+			_putchar(' ');
+		}
+		for (j = 1; j <= i; j++)
+		{
+			_putchar(35);
+		}
+		_putchar('\n');
+	}
+}
