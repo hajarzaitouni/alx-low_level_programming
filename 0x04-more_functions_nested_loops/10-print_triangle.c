dad4aa82e3f463952c690b276ff274318f8f79eb
hajar zaitouni <hajarzaitouni88@gmail.com>
2023-04-27 15:07:37 -0700

    add changes ti 10-print_triangle.c File

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -9,16 +9,22 @@ void print_triangle(int size)
 {
 	int i, j;
 
-	for (i = 1; i <= size; i++)
-	{
-		for (j = 1; j <= size - i; j++)
-		{
-			_putchar(' ');
-		}
-		for (j = 1; j <= i; j++)
-		{
-			_putchar(35);
-		}
+	if (size <= 0)
 		_putchar('\n');
+	else
+	{
+
+		for (i = 1; i <= size; i++)
+		{
+			for (j = 1; j <= size - i; j++)
+			{
+				_putchar(' ');
+			}
+			for (j = 1; j <= i; j++)
+			{
+				_putchar(35);
+			}
+			_putchar('\n');
+		}
 	}
 }
