8c2a6a2a04509521be775bf26d57ee0c2fc3ad23
hajar zaitouni <hajarzaitouni88@gmail.com>
2023-05-02 19:06:41 -0700

    update main.h and add 9-strcpy.c File

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * *_strcpy - copies the string
+ * @dest: first string parameter
+ * @src: second string parameter
+ * Return: the pointer to dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i, counter;
+
+	for (i = 0; src[i] != '\0'; i++)
+		counter++;
+	for (i = 0; i <= (counter - 1); i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[counter] = '\0';
+
+	return (dest);
+}
