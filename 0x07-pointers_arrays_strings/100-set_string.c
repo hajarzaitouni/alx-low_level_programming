30df60a34c18813a33becb940434eda287715d20
hajar zaitouni <hajarzaitouni88@gmail.com>
2023-05-08 15:45:52 -0700

    Update main.h and adding 100-set_string.c File

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * set_string - sets the value of a pointer to a char
+ * @s: double pointer
+ * @to: a pointer
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
