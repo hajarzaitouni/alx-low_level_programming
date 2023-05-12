685de7cca37927463da5874f72fc2fe187d54e09
hajar zaitouni <hajarzaitouni88@gmail.com>
2023-05-12 10:32:11 -0700

    add README.md and 0-whatsmyname.c Files

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: a program that prints its name, followed by a new line
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: 0 (Success)
+ */
+
+int main (int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
