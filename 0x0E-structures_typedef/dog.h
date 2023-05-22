ce3fbb410ec62af41549321b5a0660c1f6e7cb05
hajar zaitouni <hajarzaitouni88@gmail.com>
2023-05-22 15:36:17 -0700

    add README.md and dog.h Files

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog's informations
+ * @name: the name of the dog
+ * @age: dog's age
+ * @owner: dog's owner
+ *
+ * Description: this is about dog's world
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
