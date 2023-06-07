aff9b74e90032d892e9bf486dfe599499c9ba11a
hajar zaitouni <hajarzaitouni88@gmail.com>
2023-06-07 09:01:28 -0700

    add 100-first.c File

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+void print_first(void) __attribute__ ((constructor));
+
+/**
+ * print_first - prints some line before main function is executed
+ */
+
+void print_first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
