94c7fb8ae283120c72766182bdab5e060be82b0b
hajar zaitouni <hajarzaitouni88@gmail.com>
2023-08-11 14:41:47 -0700

    Implement a function that prints a hash table

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - prints a hash table
+ *
+ * @ht: a pointer to the hash table
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i, print_flag = 0;
+
+	if (!ht)
+		return;
+
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		if (ht->array[i])
+		{
+			if (print_flag == 1)
+				printf(" ,");
+			printf("'%s': '%s'", ht->array[i]->key, ht->array[i]->value);
+			print_flag = 1;
+			ht->array[i] = ht->array[i]->next;
+		}
+	}
+	printf("}\n");
+}
