f98a020eba38c74251d4d04c25dbf4823da8918b
Hajar Zaitouni <hajarzaitouni88@gmail.com>
2023-11-15 11:12:24 -0800

    fix some errors

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -20,7 +20,7 @@ int binary_search(int *array, size_t size, int value)
 	if (array == NULL)
 		return (-1);
 
-	while (left < right)
+	while (left <= right)
 	{
 		mid = (left + right) / 2;
 		printf("Searching in array: ");
